6e0ca69728b98430e6e651014818e7c45a7fb9af
Yevgeniy <anigeka@gmail.com>
2021-01-01 13:41:38 -0500

    refactor error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,66 @@
 class UsersController < ApplicationController
 
-    register Sinatra::Flash
+    get '/login' do 
+        clear_errors
+        
+        erb :'users/login'
+    end
+
+    post '/login' do 
+        clear_errors
+
+        @user = User.find_by(username: params[:username])
+        if @user.password == params[:password]
+            give_token
+        else
+            
+        end
+    end
 
     get '/signup' do 
-        @errors = []
+        clear_errors
+
         if session[:flash]
-            @errors = session.delete(:flash).flatten
+            add_error(session.delete(:flash))
         end
 
         erb :'users/signup'
     end
 
     post '/signup' do 
+        clear_errors
+
         username = params[:username]
         password = params[:password]
-        checked_username = proper_username?(username)
-        checked_password = proper_password?(password)
         
-        errors = []
-
-        unless username == checked_username then errors << checked_username end
-        unless password == checked_password then errors << checked_password end
+        proper_username?(username)
+        proper_password?(password)
         
-        if errors.empty? 
-            user = User.new(name: username)
-            user.password = password
-            user.save!
-            session[:user_id] = user.id
+        if @errors.empty? 
+            @user = User.new(name: username)
+            @user.password = password
+            @user.save!
+            give_token
             redirect to '/'
         else
-            session[:flash] = errors
+            session[:flash] = @errors
             redirect '/signup'
         end
     end
 
-    private
+private
+
+    def give_token
+        session[:user_id] = @user.id
+    end
+
+    def add_error(err)
+        @errors << err
+    end
+
+    def clear_errors
+        @errors = []
+    end
 
     def proper_username?(username)
         username = username.strip # Strips leading a trailing whitespace
@@ -42,21 +68,14 @@ class UsersController < ApplicationController
         
         user_exists_error = "The user by that name already exists. Please choose a different name."
         username_wrong_error = "Please make sure the length of your username is between 6 and 30 characters long and it contains no spaces."
+        if user then add_error(user_exists_error) end
+        if username.length < 6 || username.match(/\s/) then add_error(username_wrong_error) end
 
-        errors = []
-
-        if user then errors << user_exists_error end
-        if username.length < 6 || username.match(/\s/) then errors << username_wrong_error end
-
-        return_value = errors.empty? ? username : errors
-        return_value
     end
 
     def proper_password?(password)
         unless password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
-            "The password must be between 8 and 30 characters long and contain at least one letter and number"
-        else
-            password
+            add_error("The password must be between 8 and 30 characters long and contain at least one letter and number")
         end
     end
 
