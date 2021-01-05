922f12ee26d0c5a9ba4df713edd157667109ffb6
Yevgeniy <anigeka@gmail.com>
2021-01-05 13:52:56 -0500

    remove unnecessary function in search

--- public/javascripts/search-bar.js
+++ public/javascripts/search-bar.js
@@ -55,35 +55,3 @@ function appendNames(bookNames, ul) {
         ul.appendChild(listItem);
     }
 }
-
-function blah() {
-    searchBar.addEventListener('keyup', (e) => {
-        getBooks(results);
-        results.innerHTML = "";
-        let listItem = document.getElementsByClassName('list-item');
-        $.get("/test", (data) => {
-            const parsedData = JSON.parse(data);
-            const bookNames = parsedData.names;
-
-            for (let i = 0; i < bookNames.length; i++) {
-                const listItem = document.createElement("LI");
-                listItem.classList.add('list-item');
-                listItem.classList.add(`item-${i + 1}`);
-
-                const bookLink = document.createElement("A");
-                bookLink.classList.add('book-link');
-                bookLink.classList.add(`link-${i + 1}`);
-                bookLink.href = `/books/${bookNames[i]}`;
-
-                const bookName = document.createElement("P");
-                bookName.classList.add('book-name');
-                bookName.classList.add(`name-${i + 1}`);
-                bookName.innerHTML = bookNames[i];
-
-                bookLink.appendChild(bookName);
-                listItem.appendChild(bookLink);
-                results.appendChild(listItem);
-            }
-        });
-    });
-}
