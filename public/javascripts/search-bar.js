fb85e4095b064e254e95ae54a6159301bdf211c2
Yevgeniy <anigeka@gmail.com>
2021-01-05 13:50:56 -0500

    make search request asyncronous

--- public/javascripts/search-bar.js
+++ public/javascripts/search-bar.js
@@ -1,42 +1,89 @@
 window.onload = (e) => {
     const searchBar = document.getElementById('search-bar');
     if (searchBar) {
-        // Keydown shouldn't fire if nothing is present in the field
-        searchBar.addEventListener('keydown', (e) => {           
-                let results = document.getElementById('search-results');
-                let listItem = document.getElementsByClassName('list-item');
-                console.log(listItem.length)
-
-                $.get("/test", (data) => {
-                    const parsedData = JSON.parse(data);
-                    const bookNames = parsedData.names;
-
-                    for(let i = 0; i < bookNames.length; i++){
-                        const listItem = document.createElement("LI");
-                        listItem.classList.add('list-item');
-                        listItem.classList.add(`item-${i+1}`);
-
-                        const bookLink = document.createElement("A");
-                        bookLink.classList.add('book-link');
-                        bookLink.classList.add(`link-${i+1}`);
-                        bookLink.href = `/books/${bookNames[i]}`;
-
-                        const bookName = document.createElement("P");
-                        bookName.classList.add('book-name');
-                        bookName.classList.add(`name-${i+1}`);
-                        bookName.innerHTML = bookNames[i];
-
-                        bookLink.appendChild(bookName);
-                        listItem.appendChild(bookLink);
-                        results.appendChild(listItem);
-                    }
-                });
+        let timeOut;
+        searchBar.addEventListener('keyup', (e) => {
+            if (timeOut) { clearTimeout(timeOut); }
+            timeOut = setTimeout(() => { makeRequest(searchBar.value); }, 1000);
         });
     }
 }
 
-function removeAllLiNodes(parent) {
-    while(parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+function makeRequest(enteredInfo) {
+    getBooks(enteredInfo);
+}
+
+async function getBooks(name) {
+    let listItem = document.getElementById('search-results');
+    let result;
+
+    try {
+        result = await $.ajax({
+            url: '/test',
+            type: 'GET',
+            data: { 'name': name },
+            success: function (data, status, xhr) {
+                appendNames(data, listItem);
+            },
+            dataType: "json"
+        });
+    } catch (e) {
+
     }
+}
+
+function appendNames(bookNames, ul) {
+    ul.innerHTML = "";
+    bookNames = bookNames.names;
+    for (let i = 0; i < bookNames.length; i++) {
+        const listItem = document.createElement("LI");
+        listItem.classList.add('list-item');
+        listItem.classList.add(`item-${i + 1}`);
+
+        const bookLink = document.createElement("A");
+        bookLink.classList.add('book-link');
+        bookLink.classList.add(`link-${i + 1}`);
+        bookLink.href = `/books/${bookNames[i]}`;
+
+        const bookName = document.createElement("P");
+        bookName.classList.add('book-name');
+        bookName.classList.add(`name-${i + 1}`);
+        bookName.innerHTML = bookNames[i];
+
+        bookLink.appendChild(bookName);
+        listItem.appendChild(bookLink);
+        ul.appendChild(listItem);
+    }
+}
+
+function blah() {
+    searchBar.addEventListener('keyup', (e) => {
+        getBooks(results);
+        results.innerHTML = "";
+        let listItem = document.getElementsByClassName('list-item');
+        $.get("/test", (data) => {
+            const parsedData = JSON.parse(data);
+            const bookNames = parsedData.names;
+
+            for (let i = 0; i < bookNames.length; i++) {
+                const listItem = document.createElement("LI");
+                listItem.classList.add('list-item');
+                listItem.classList.add(`item-${i + 1}`);
+
+                const bookLink = document.createElement("A");
+                bookLink.classList.add('book-link');
+                bookLink.classList.add(`link-${i + 1}`);
+                bookLink.href = `/books/${bookNames[i]}`;
+
+                const bookName = document.createElement("P");
+                bookName.classList.add('book-name');
+                bookName.classList.add(`name-${i + 1}`);
+                bookName.innerHTML = bookNames[i];
+
+                bookLink.appendChild(bookName);
+                listItem.appendChild(bookLink);
+                results.appendChild(listItem);
+            }
+        });
+    });
 }
